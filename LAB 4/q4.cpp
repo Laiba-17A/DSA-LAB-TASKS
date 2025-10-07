28657ce0aeebde3844c6c5291b109ad128b5a01b
Laiba-17A <laiba17javed@gmail.com>
2025-10-07 19:01:36 +0500

    Update q4.cpp

--- LAB 4/q4.cpp
+++ LAB 4/q4.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void sortArray(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 bool hasDup(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (arr[i] == arr[j]) {
-                return true;
-            }
+        if (arr[i] == arr[i + 1]) {
+            return true;
         }
     }
     return false;
@@ -15,13 +26,19 @@ bool hasDup(int arr[], int size) {
 int main() {
     int arr[] = {5, 3, 8, 2, 5, 9};
     int size = sizeof(arr) / sizeof(arr[0]);
+    sortArray(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     if (hasDup(arr, size)) {
-        cout << "Array has duplicates" << endl;
+        cout << "Array has duplicates." << endl;
     } else {
         cout << "Array does not have duplicates." << endl;
     }
 
     return 0;
 }
-
